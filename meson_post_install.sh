b6056104b38d19b8ffece356c9419c5efc7c5325
Callum Farmer <gmbr3@opensuse.org>
2023-02-14 22:23:39 +0000

    Install sh clarity
    Signed-off-by: Callum Farmer <gmbr3@opensuse.org>

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -7,13 +7,15 @@ upstream_file_one="$schema_path/90_budgie_lightdm.gschema.override"
 upstream_file_two="$schema_path/90_budgie_settings.gschema.override"
 upstream_file_name="$schema_path/90_budgie_gnome_settings.gschema.override"
 new_file_name="$schema_path/21_budgie_gnome_settings_upstream.gschema.override"
+dist_logo_dir="$DEST/$PREFIX/share/icons/hicolor/scalable/apps"
+opensuse_logo="$dist_logo_dir/budgie-openSUSE-distributor-logo.svg"
+upstream_logo="$dist_logo_dir/budgie-upstream-distributor-logo.svg"
 if [ -e $upstream_file_name ]; then
 	cat $upstream_file_one >> $upstream_file_name
 	cat $upstream_file_two >> $upstream_file_name
 	rm $upstream_file_one
 	rm $upstream_file_two
 	mv $upstream_file_name $new_file_name
-	sed 's:/usr/share/pixmaps/fedora-logo-sprite.svg:/usr/share/icons/hicolor/scalable/apps/budgie-upstream-distributor-logo.svg:g' -i $schema_path/21_budgie_gnome_settings_upstream.gschema.override
-	cp $DEST/usr/share/icons/hicolor/scalable/apps/budgie-openSUSE-distributor-logo.svg $DEST/usr/share/icons/hicolor/scalable/apps/budgie-upstream-distributor-logo.svg
-
+	sed 's:/usr/share/pixmaps/fedora-logo-sprite.svg:/usr/share/icons/hicolor/scalable/apps/budgie-upstream-distributor-logo.svg:g' -i $new_file_name
+	cp $opensuse_logo $upstream_logo
 fi
