29177fa1f20d411f3878fe5f9ac903fb88cc8c2f
Callum Farmer <gmbr3@opensuse.org>
2023-02-14 21:21:56 +0000

    Fix install sh
    Signed-off-by: Callum Farmer <gmbr3@opensuse.org>

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+DEST=${DESTDIR:-.}
 PREFIX=${MESON_INSTALL_PREFIX:-/usr}
-schema_path="$PREFIX/share/glib-2.0/schemas"
+schema_path="$DEST/$PREFIX/share/glib-2.0/schemas"
 upstream_file_name="$schema_path/90_budgie_settings.gschema.override"
 new_file_name="$schema_path/21_budgie_gnome_settings_upstream.gschema.override"
 if [ -e $upstream_file_name ]; then
